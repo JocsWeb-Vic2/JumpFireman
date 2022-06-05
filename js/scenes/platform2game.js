94f422e4614b964cd5fba361ff9ed5626f84b016
Pau Garrido <paugarrido2000@gmail.com>
2022-06-05 21:25:37 +0200

    Update

--- js/scenes/platform2game.js
+++ js/scenes/platform2game.js
@@ -1,4 +1,6 @@
-"use strict";
+  "use strict";
+
+var pausat = -1;
 
 class Platform2Scene extends Phaser.Scene {
     constructor (){
@@ -14,27 +16,28 @@ class Platform2Scene extends Phaser.Scene {
 		this.gameOver = false;
 		this.newfons = null;
     }
-    preload (){	
+    preload (){
 		this.load.image('sky', '../resources/starsassets/fons.png');
 		this.load.image('ground', '../resources/starsassets/platform2.png');
 		this.load.image('star', '../resources/starsassets/star.png');
 		this.load.image('bomb', '../resources/starsassets/bomb.png');
-		
+
 		this.load.spritesheet('dude',
 			'../resources/starsassets/dude2.png',
 			{ frameWidth: 32, frameHeight: 32 }
 		);
 	}
-    create (){	
+    create (){
 
-		
+
 		{	// Creem platafomress
 
-			this.newfons = this.fons.create(450, 290, 'sky');
-			this.newfons.setVelocityY(100);
+      this.fons = this.physics.add.group();
+			this.newfons = this.fons.create(400, 250, 'sky');
+			this.newfons.setVelocityY(300);
 
 			this.platforms = this.physics.add.staticGroup();
-            this.platforms.create(0, 600, 'ground').setScale(0.2, 0.1).refreshBody();
+      this.platforms.create(0, 600, 'ground').setScale(0.2, 0.1).refreshBody();
 			this.platforms.create(200, 600, 'ground').setScale(0.2, 0.1).refreshBody();
 			this.platforms.create(400, 600, 'ground').setScale(0.2, 0.1).refreshBody();
 			this.platforms.create(600, 600, 'ground').setScale(0.2, 0.1).refreshBody();
@@ -45,14 +48,24 @@ class Platform2Scene extends Phaser.Scene {
 			this.Levelplatforms.create(400, 300, 'ground').setScale(0.1, 0.075).refreshBody();
 			this.Levelplatforms.create(600, 400, 'ground').setScale(0.1, 0.075).refreshBody();
 			this.Levelplatforms.create(800, 500, 'ground').setScale(0.1, 0.075).refreshBody();
-			
-        //this.platforms.create(750, 220, 'ground').setScale(0.1).refreshBody();
+
 		}
+
+
 		{	// Creem player i definim animacions
 			this.player = this.physics.add.sprite(100, 450, 'dude');
 			this.player.setBounce(0.2);
 			this.player.setCollideWorldBounds(true);
-			
+
+      var button_pause = this.add.text(400, 550, 'Pause') //el botó de pause game, que és un text que s'activa al clickar a sobre.
+				.setOrigin(0.5)
+				.setPadding(10)
+				.setStyle({ backgroundColor: '#111' })
+				.setInteractive({ useHandCursor: true })
+				.on('pointerdown', ()=>{
+					pausat = 0;
+				});
+
 			this.anims.create({
 				key: 'left',
 				frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 1 }),
@@ -73,13 +86,14 @@ class Platform2Scene extends Phaser.Scene {
 				repeat: -1
 			});
 		}
+    
 		{	// Creem objectes interactuables
 			this.stars = this.physics.add.group({
 				key: 'star',
 				repeat: 11,
 				setXY: { x: 12, y: 0, stepX: 70 }
 			});
-			this.stars.children.iterate((child) => 
+			this.stars.children.iterate((child) =>
 				child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8)));
 		}
 
@@ -92,19 +106,19 @@ class Platform2Scene extends Phaser.Scene {
 			this.physics.add.collider(this.player, this.Levelplatforms);
 			this.physics.add.collider(this.stars, this.Levelplatforms);
 			this.cursors = this.input.keyboard.createCursorKeys();
-			this.physics.add.overlap(this.player, this.stars, 
+			this.physics.add.overlap(this.player, this.stars,
 				(body1, body2)=>this.collectStar(body1, body2));
 			this.physics.add.collider(this.bombs, this.platforms);
 			this.physics.add.collider(this.bombs, this.Levelplatforms);
-			this.physics.add.collider(this.player, this.bombs, 
+			this.physics.add.collider(this.player, this.bombs,
 				(body1, body2)=>this.hitBomb(body1, body2));
 		}
 		{ // UI
-			this.scoreText = this.add.text(16, 16, 'Score: 0', 
+			this.scoreText = this.add.text(16, 16, 'Score: 0',
 				{ fontSize: '32px', fill: '#000' });
 		}
 	}
-	update (){	
+	update (){
 		if (this.gameOver) return;
 		{ // Moviment
 			if (this.cursors.left.isDown){
@@ -127,6 +141,11 @@ class Platform2Scene extends Phaser.Scene {
 			if(this.newfons.y == 650){
 				this.newfons.y = 0;
 			}
+
+      if(pausat === 0){ //si hem pausat es para l'escena i llançem la de pausa
+				this.scene.pause();
+				this.scene.launch('escena_pausa');
+			}
 		}
 	}
 	collectStar(player, star){
@@ -146,7 +165,7 @@ class Platform2Scene extends Phaser.Scene {
         bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
 	}
 	hitBomb(player, bomb){
-		if (this.gameOver) 
+		if (this.gameOver)
 			return;
 		this.physics.pause();
 		this.player.setTint(0xff0000);
@@ -155,7 +174,7 @@ class Platform2Scene extends Phaser.Scene {
 		setTimeout(()=>loadpage("../"), 3000);
 	}
 	enableAllStars(){
-		this.stars.children.iterate(child => 
+		this.stars.children.iterate(child =>
 			child.enableBody(true, child.x, 0, true, true));
 	}
 
@@ -168,4 +187,3 @@ class Platform2Scene extends Phaser.Scene {
 	}
 
 }
-
